44c17d119787dc4eac456c02f5d97c51b36341dd
nosiee <nosiee.nosiee@gmail.com>
2023-02-16 01:00:07 +0300

    some fixes

--- denv.sh
+++ denv.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 
-build () {
-    docker build -f $1 . -t $2
+args=()
+file=""
+image=""
+name=""
+gui=false
+port=""
 
+for v in "$@"; do
+    args+=($v)
+done
+
+for (( i=1; i <= ${#args[@]}; i++ )); do
+    case ${args[$i]} in
+        --file)
+            file=${args[$i+1]}
+            ;;
+        --image)
+            image=${args[$i+1]}
+            ;;
+        --name)
+            name=${args[$i+1]}
+            ;;
+        --gui)
+            gui=true
+            ;;
+        --port)
+            port=${args[$i+1]}
+            ;;
+    esac
+done
+
+build() {
+    if [[ $file == *"/"* ]]
+    then
+        filename=$(echo $file | rev | cut -d '/' -f 1 | rev)
+        fullpath=${file%"$filename"}
+
+        docker build --file $file $fullpath -t $name
+    else
+        docker build --file $file . -t $name
+    fi
+}
+
+add() {
     cmd="docker create"
     it="-it"
-    p="-p 8080:8080"
+
+    if [[ $port == "" ]]
+    then
+        port="-p 8080:8080"
+    fi
+
     config_volumes="-v /home/nosiee/.config/nvim:/home/nosiee/.config/nvim
                     -v /home/nosiee/.local/share/nvim:/home/nosiee/.local/share/nvim
                     -v /home/nosiee/.local/state/nvim:/home/nosiee/.local/state/nvim
@@ -14,50 +60,34 @@ build () {
                     -v /home/nosiee/.gitconfig:/home/nosiee/.gitconfig"
 
     network="--network bridge"
-    name="--name $3 $2"
+    container_name="--name $name $image"
 
-    if [ "$4" = "gui" ]
+    if [ $gui == true ]
     then
         env="-e XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR} -e DISPLAY=${DISPLAY}"
         xorg_volume="-v /tmp/.X11-unix:/tmp/.X11-unix"
         network="--network host"
         p=""
 
-        token=$(xauth list)
-        echo -e "\nAdd xauth token to your container: xauth add ${token}"
+        echo -e "Add xauth token to your container: xauth add $(xauth list)"
     fi
 
-    container_id=$($cmd $env $it $p $config_volumes $xorg_volume $network $name)
-    echo "Use 'denv run $3/${container_id}' to run the container"
+    container_id=$($cmd $env $it $p $config_volumes $xorg_volume $network $container_name)
+    echo "Use 'denv run $name/${container_id}' to run the container"
 }
 
-run () {
-    docker start -i $1
+run() {
+    docker container start -i $1
 }
 
-prune() {
-    echo "Are you sure you want to delete all stopped containers?"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes) 
-                docker rm $(docker ps --filter status=exited -q)
-                break
-                ;;
-            No)
-                exit
-                ;;
-        esac
-    done
-}
-
-case $1 in
+case ${args[0]} in
     build)
-        build $2 $3 $4 $5
+        build
+        ;;
+    add)
+        add
         ;;
     run)
         run $2
-        ;;
-    prune)
-        prune
         ;;
 esac
